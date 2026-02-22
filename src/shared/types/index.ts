0a7816df77841e675df1a04c0ff318f632ee3be7
mrkadirov2005 <muzaffar571181@gmail.com>
2026-02-23 01:33:46 +0500

    [PANTHERS] Removed Unneccessary types

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,2 +1,1 @@
 export type { InvitesTableType, InvitesInsertType } from '#/modules/database/schema/invites.schema';
-export type { InviteRole } from './invites.types';
--- src/shared/types/invites.types.ts
+++
@@ -1,1 +0,0 @@
-export type InviteRole = 'admin' | 'hr' | 'instructor' | 'student';
