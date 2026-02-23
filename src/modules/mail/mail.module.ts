c1e90a537705bbd2d67e8f85cc157c6b0d4d6bf8
Fayzulla-Shamsiev <129758929+Fayzulla-Shamsiev@users.noreply.github.com>
2026-02-23 16:56:45 +0500

    Aligned Modules stricly to types from env config

--- src/modules/mail/mail.module.ts
+++ src/modules/mail/mail.module.ts
@@ -1,31 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { MailerModule } from '@nestjs-modules/mailer';
+import type { EnvConfig } from '#/shared/configs';
 import { MailService } from './mail.service';
 
 @Module({
   imports: [
     MailerModule.forRootAsync({
       inject: [ConfigService],
-      useFactory: (config: ConfigService) => {
-        const port = Number(config.get<string>('MAIL_SMTP_PORT') ?? '587');
-        const secure = (config.get<string>('MAIL_SMTP_SECURE') ?? '').toLowerCase() === 'true';
-
-        const host = config.get<string>('MAIL_SMTP_HOST');
-        if (!host) throw new Error('MAIL_SMTP_HOST is required');
-
-        const user = config.get<string>('MAIL_SMTP_USER') ?? '';
-        const pass = config.get<string>('MAIL_SMTP_PASS') ?? '';
+      useFactory: (configService: ConfigService<EnvConfig>) => {
+        const host = configService.get('MAIL_SMTP_HOST', { infer: true });
+        const port = configService.get('MAIL_SMTP_PORT', { infer: true });
 
         return {
           transport: {
             host,
             port,
-            secure,
-            ...(user && pass ? { auth: { user, pass } } : {}),
-          },
-          defaults: {
-            from: config.get<string>('MAIL_FROM') ?? 'no-reply@example.com',
           },
         };
       },
