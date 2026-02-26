44a00f905f5775e276424b7ce5593229ce5ec909
mrkadirov2005 <muzaffar571181@gmail.com>
2026-02-26 14:41:43 +0500

    [PANTHERS-12] Updated migration file, removed unneccessary comments

--- migrations/files/20260220195805-invites-table.js
+++ migrations/files/20260220195805-invites-table.js
@@ -1,59 +1,57 @@
 'use strict';
+
+const upSql = `
+DO $$
+BEGIN
+  IF NOT EXISTS (
+    SELECT 1 FROM pg_type WHERE typname = 'user_role'
+  ) THEN
+    CREATE TYPE user_role AS ENUM (
+      'admin',
+      'hr',
+      'instructor',
+      'student'
+    );
+  END IF;
+END$$;
+
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE IF NOT EXISTS "invites" (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+
+  email TEXT NOT NULL,
+
+  first_name TEXT,
+  last_name TEXT,
+
+  role user_role NOT NULL,
+
+  token TEXT NOT NULL,
+
+  expires_at TIMESTAMPTZ NOT NULL,
+  used_at TIMESTAMPTZ,
+  revoked_at TIMESTAMPTZ,
+
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+`;
+
+const downSql = `
+DROP TABLE IF EXISTS "invites";
+DROP TYPE IF EXISTS user_role;
+`;
+
 module.exports = {
   async up(queryInterface) {
     await queryInterface.sequelize.transaction(async (transaction) => {
-      await queryInterface.sequelize.query(`
-        DO $$
-        BEGIN
-          IF NOT EXISTS (
-            SELECT 1 FROM pg_type WHERE typname = 'user_role'
-          ) THEN
-            CREATE TYPE user_role AS ENUM (
-              'admin',
-              'hr',
-              'instructor',
-              'student'
-            );
-          END IF;
-        END$$;
-      `, { transaction });
-
-      await queryInterface.sequelize.query(`
-        CREATE EXTENSION IF NOT EXISTS "pgcrypto";
-      `, { transaction });
-
-      await queryInterface.sequelize.query(`
-        CREATE TABLE IF NOT EXISTS "invites" (
-          id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-
-          email TEXT NOT NULL,
-
-          first_name TEXT,
-          last_name TEXT,
-
-          role user_role NOT NULL,
-
-          token TEXT NOT NULL,
-
-          expires_at TIMESTAMPTZ NOT NULL,
-          used_at TIMESTAMPTZ,
-          revoked_at TIMESTAMPTZ,
-
-          created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-        );
-      `, { transaction });
+      await queryInterface.sequelize.query(upSql, { transaction });
     });
   },
 
   async down(queryInterface) {
     await queryInterface.sequelize.transaction(async (transaction) => {
-      await queryInterface.sequelize.query(`
-        DROP TABLE IF EXISTS "invites";
-      `, { transaction });
-
-      await queryInterface.sequelize.query(`
-        DROP TYPE IF EXISTS user_role;
-      `, { transaction });
+      await queryInterface.sequelize.query(downSql, { transaction });
     });
   }
 };
